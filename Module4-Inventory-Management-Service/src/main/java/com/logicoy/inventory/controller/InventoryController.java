6f356d8de7195d58448c34d1e0655912db5acea4
Mahesh <mahesh.rathod@logicoy.com>
2026-01-30 19:11:18 +0530

    Add InventoryController for inventory stock management

--- Module4-Inventory-Management-Service/src/main/java/com/logicoy/inventory/controller/InventoryController.java
+++ Module4-Inventory-Management-Service/src/main/java/com/logicoy/inventory/controller/InventoryController.java
@@ -5,20 +5,20 @@ import org.springframework.web.bind.annotation.*;
 import com.logicoy.inventory.service.InventoryService;
 
 @RestController
-@RequestMapping("/inventory")
+@RequestMapping("/inventory")// Base URL for all inventory-related endpoints
 public class InventoryController {
 
+	 // Service layer dependency to handle business logic
     private final InventoryService inventoryService;
 
     public InventoryController(InventoryService inventoryService) {
         this.inventoryService = inventoryService;
     }
-
+    
+    // API to reduce product stock based on product ID and quantity
     @PostMapping("/{productId}/reduce")
-    public String reduceStock(
-            @PathVariable Long productId,
-            @RequestParam int quantity) {
-
+    public String reduceStock(@PathVariable Long productId,@RequestParam int quantity) 
+    {
         inventoryService.reduceStock(productId, quantity);
         return "Stock updated successfully";
     }
