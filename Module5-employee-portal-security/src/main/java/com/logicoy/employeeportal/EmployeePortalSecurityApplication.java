eb74b261f74966d07317e4c5909685849c864013
Mahesh <mahesh.rathod@logicoy.com>
2026-02-04 16:54:07 +0530

    Module 5: add initial package structure

---
+++ Module5-employee-portal-security/src/main/java/com/logicoy/employeeportal/EmployeePortalSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.logicoy.employeeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePortalSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePortalSecurityApplication.class, args);
+	}
+
+}
---
+++ Module5-employee-portal-security/src/test/java/com/logicoy/employeeportal/EmployeePortalSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logicoy.employeeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePortalSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
